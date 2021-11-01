d0839be99d644f7a53fa87a7b0fe2975e8a2cad2
honer.liberal <honer.liberal@neon.com.br>
2021-10-31 21:24:44 -0300

    login.html funcional. tudo ok. Preciso de ajuda no tamanho do botão entrar e posicionamento

--- loginbackup.js
+++ loginbackup.js
@@ -1,17 +1,16 @@
 document.querySelector('#email').addEventListener('blur', validateEmail);
 document.querySelector('#password').addEventListener('blur', validatePassword);
-const reSpaces = /^\S*$/;
 
+const reSpaces = /^\S*$/;
 function validateEmail(e) {
   const email = document.querySelector('#email');
   const re = /^([a-zA-Z0-9_\-?\.?]){3,}@([a-zA-Z]){3,}\.([a-zA-Z]){2,5}$/;
-  const ree = /^([a-zA-Z0-9_\-?\.?]){3,}@([a-zA-Z]){3,}\.([a-zA-Z]){2,5}\.([a-zA-Z]){2,5}$/;
 
-  if (reSpaces.test(email.value) && (re.test(email.value) || ree.test(email.value))) {
-      email.classList.remove('is-invalid');
-      email.classList.add('is-valid');
-      return true;
-      
+  if (reSpaces.test(email.value) && re.test(email.value)) {
+    email.classList.remove('is-invalid');
+    email.classList.add('is-valid');
+
+    return true;
   } else {
     email.classList.add('is-invalid');
     email.classList.remove('is-valid');
@@ -47,7 +46,6 @@ function validatePassword() {
         ) {
           event.preventDefault();
           event.stopPropagation();
-          
         } else {
           form.classList.add('was-validated');
         }
