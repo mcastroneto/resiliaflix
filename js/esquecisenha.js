79dc0493c0f609dfe5f98801408c0684a657de0a
honer.liberal <honer.liberal@neon.com.br>
2021-11-02 18:28:15 -0300

    erro footer arrumado. Ver css login.css - main min-height: 400px;

--- js/esquecisenha.js
+++ js/esquecisenha.js
@@ -42,11 +42,3 @@ function validateEmail(e) {
     );
   }
 })();
-
-window.onload = function () { var images = ['bk/01.jpg', 'bk/02.jpg', 'bk/03.jpg', 'bk/04.jpg', 'bk/05.jpg', 'bk/06.jpg', 'bk/07.jpg', 'bk/08.jpg', 'bk/09.jpg', 'bk/10.jpg', 'bk/11.jpg', 'bk/12.jpg', 'bk/13.jpg', 'bk/14.jpg'];
-var image = images[Math.floor(Math.random() * images.length)];
-document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
-document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
-document.getElementsByTagName('body')[0].style.backgroundPositionX = "center";
-document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
-}
--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-window.onload = function () { var images = ['bk/01.jpg', 'bk/02.jpg', 'bk/03.jpg', 'bk/04.jpg', 'bk/05.jpg', 'bk/06.jpg', 'bk/07.jpg', 'bk/08.jpg', 'bk/09.jpg', 'bk/10.jpg', 'bk/11.jpg', 'bk/12.jpg', 'bk/13.jpg', 'bk/14.jpg'];
+window.onload = function () { var images = ['imagens/bk/01.jpg', 'imagens/bk/02.jpg', 'imagens/bk/03.jpg', 'imagens/bk/04.jpg'];
 var image = images[Math.floor(Math.random() * images.length)];
 document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
 document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
