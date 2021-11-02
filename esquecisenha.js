37841ac1698cbfb784fdfa90b842576ce9e8367b
honer.liberal <honer.liberal@neon.com.br>
2021-11-02 13:47:16 -0300

    footer pronto para avaliação do grupo

--- esquecisenha.js
+++ esquecisenha.js
@@ -47,5 +47,6 @@ window.onload = function () { var images = ['bk/01.jpg', 'bk/02.jpg', 'bk/03.jpg
 var image = images[Math.floor(Math.random() * images.length)];
 document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
 document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
+document.getElementsByTagName('body')[0].style.backgroundPositionX = "center";
 document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
 }
